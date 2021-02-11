0ef8dc104f785b5aec312218465d282ff166be3e
David S Czulada <dczulada@mitre.org>
2021-02-11 13:43:44 -0500

    more fhir stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,12 @@ class WelcomeController < ApplicationController
   	entry = Rails.root.join('tmp', 'patient-bundle.json')
   	@source_patient = JSON.parse(File.read(entry), max_nesting: 100)
   	@patient_resource = @source_patient['entry'].find { |e| e['resource']['resourceType'] == 'Patient' }['resource']
+    bundle = FHIR::Bundle.new(@source_patient)
   	allResources = @source_patient['entry'].map { |e| e['resource'] }
   	@conditions = allResources.select { |e| e['resourceType'] == 'Condition' }
   	@encounters = allResources.select { |e| e['resourceType'] == 'Encounter' }
   	@immunizations = allResources.select { |e| e['resourceType'] == 'Immunization' }
+  	@single = @immunizations.first['resource']
+    @errors = bundle.validate
   end
 end
--- app/javascript/components/Fhir.js
+++ app/javascript/components/Fhir.js
@@ -1,16 +1,22 @@
 var React = require("react")
 var PropTypes = require("prop-types")
 var FhirVisualizers = require("fhir-visualizers")
-
+var ObjectInspector = require("react-object-inspector")
 
 class Fhir extends React.Component {
+
   render () {
     return (
       <div>
+        <ObjectInspector data={ this.props.errors } />
         <FhirVisualizers.PatientVisualizer patient={this.props.patient} />
+        <ObjectInspector data={ this.props.patient } />
         <FhirVisualizers.EncountersVisualizer rows={this.props.encounters} />
+        <ObjectInspector data={ this.props.encounters } />
         <FhirVisualizers.ConditionsVisualizer rows={this.props.conditions} />
+        <ObjectInspector data={ this.props.conditions } />
         <FhirVisualizers.ImmunizationsVisualizer rows={this.props.immunizations} />
+        <ObjectInspector data={ this.props.immunizations } />
       </div>
     );
   }
